eb0347e74f292c7294b210b751c5662837fd1cb2
hddkkll <shs403102@naver.com>
2019-06-17 23:59:14 +0900

    행렬곱

---
+++ src/mul_array.java
@@ -0,0 +1,25 @@
+/*
+
+ */
+public class mul_array {
+	public static void main(String[] args) {
+		Solution10 s = new Solution10();
+		
+	}
+}
+class Solution10{
+	public int[][] solution(int[][] arr1, int[][]arr2){
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr2[0].length; j++) {
+				int temp=0;
+				for(int k=0; k<arr1[0].length; k++) {
+					temp+=arr1[i][k]*arr2[k][j];
+				}
+				answer[i][j]=temp;
+			}
+		}
+		return answer;
+	}
+}
